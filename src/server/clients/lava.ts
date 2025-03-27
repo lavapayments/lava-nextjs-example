9a2ee3c844e654d451ff5fc4753686f4a52a806f
Richard Hristov <richardhristov@gmail.com>
2025-03-27 19:42:32 +0200

    update sdk version

--- src/server/clients/lava.ts
+++ src/server/clients/lava.ts
@@ -2,5 +2,5 @@ import { Lava } from "@lavapayments/nodejs";
 import { env } from "~/env";
 
 export const lava = new Lava(env.LAVA_SECRET_KEY, {
-  apiVersion: "2025-03-10.v1",
+  apiVersion: "2025-03-27.v1",
 });
